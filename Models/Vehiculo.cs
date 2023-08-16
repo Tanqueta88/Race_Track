dd68c0f25a4cfdb1d6b1bad0513d5bacd6ad4097
tanqueta88 <oliverprim783@gmail.com>
2023-08-16 17:09:24 -0300

    Models Piloto and Vehiculo is added

--- Models/Vehiculo.cs
+++ Models/Vehiculo.cs
@@ -7,6 +7,13 @@ namespace Race_Track.Models
 {
     public class Vehiculo
     {
-        
+        public int VehiculoId { get; set; }
+        public string? VehiculoNombre {get; set;}
+        public string? VehiculoApellido {get; set;}
+        public string? VehiculoTipo {get; set;}
+        public string? VehiculoMatricula {get; set;}
+        public DateTime VehiculoFabricacion {get;set;}  
+        // public int AeronaveCosto {get; set;}
+        public ICollection<Piloto> PilotoList { get; set; } = new List<Piloto>();      
     }
 }
